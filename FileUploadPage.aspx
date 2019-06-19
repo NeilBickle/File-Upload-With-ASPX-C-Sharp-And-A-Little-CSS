7d63d33798c4c965625a88288652624c67f18307
Neil Bickle (AMBCS) (AssocIScT) (TMIET) (ICTTech) <43986316+NeilBickle@users.noreply.github.com>
2019-06-19 09:28:50 +0100

    You Will Have To Create A Folder Called Images
    You Will Have To Create A Folder Called Images In The Directory You Put The Program, For The Program To Work, Github Does Not Make The Folder When Uploading.

---
+++ FileUploadPage.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="FileUploadPage.aspx.cs" Inherits="FileUploadPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>FileUpload</title>
+    <style>
+        body {
+        background-color: beige;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>File Upload In ASPX Program</h1>
+        <table style="width: 45%; border: 10px solid black;">
+            <tr>
+                <td><asp:Label ID="LblInfo" runat="server" Text="Select A File To Upload:" Enabled="False"></asp:Label><asp:FileUpload ID="FileUploader" runat="server" /></td>
+            </tr>      
+            <tr>
+                <td><asp:Label ID="LblUploadInfo" runat="server" Text="Click To Upload:" Enabled="False"></asp:Label><asp:Button ID="BtnUpload" runat="server" Text="Upload File" onclick="BtnUpload_Click" /></td>
+            </tr>
+            <tr>
+                <td><asp:Label runat="server" id="LblStatus" text="Status Of Upload: " /></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ FileUploadPage.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class FileUploadPage : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnUpload_Click(object sender, EventArgs e)
+    {
+        LblStatus.Text = "";
+        if (FileUploader.HasFile)
+        {
+            string ext = System.IO.Path.GetExtension(FileUploader.FileName);
+            
+                    if (ext ==".jpg" || ext == ".png" || ext == ".bmp" || ext == ".txt" || ext == ".jpeg" || ext == ".gif" || ext == ".PNG" || ext == ".JPG" || ext == ".JPEG")
+                    {
+                        string Path = Server.MapPath("Images\\");
+                        string file = FileUploader.FileName;
+                        FileUploader.SaveAs(Path + file);
+                        LblStatus.Text = "Upload status: File uploaded!";
+                    }
+                    else
+                        LblStatus.Text = "Upload status: The file could not be uploaded.";
+            
+        }
+    }
+}
